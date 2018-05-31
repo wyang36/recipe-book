cb26720fe2a3488815b7f3d857514f719ac43960
Kira Yang <wenqin.z.yang@gmail.com>
2018-05-30 21:57:48 -0400

    spliting modules
    separating into different modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,24 +2,12 @@ import { Routes, RouterModule } from "@angular/router";
 import { RecipesComponent } from "./recipes/recipes.component";
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component"
 import { NgModule } from "@angular/core";
-import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
-import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.component";
-import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
 import { SignupComponent } from "./auth/signup/signup.component";
 import { SigninComponent } from "./auth/signin/signin.component";
 import { AuthGuard } from "./auth/auth-guard.service";
 
 const appRoutes: Routes = [
-    {path: '', redirectTo: '/recipes', pathMatch: 'full'},
-    {path: 'recipes', component: RecipesComponent, children: [
-        {path: '', component: RecipeStartComponent},
-        {path: 'new', component: RecipeEditComponent, canActivate: [AuthGuard]},
-        {path: ':id', component: RecipeDetailComponent},
-        {path: ':id/edit', component: RecipeEditComponent, canActivate: [AuthGuard]}
-    ]},
-    {path: 'shopping-list', component: ShoppingListComponent},
-    {path: 'signup', component: SignupComponent},
-    {path: 'signin', component: SigninComponent}
+    {path: '', redirectTo: '/recipes', pathMatch: 'full'}
 ]
 
 @NgModule({
